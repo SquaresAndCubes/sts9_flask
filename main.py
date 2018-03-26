611957b8b6858a5a5c1262611a7f59cadb70486b
c_bvaalb <btvaalburg@gmail.com>
2018-03-26 17:04:50 -0600

    Add - nav buttons, url routing

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_pymongo import PyMongo
+import datetime
 sts9 = Flask(__name__, )
 
+#bootstrap css object
 bootstrap = Bootstrap(sts9)
 
 #sts9 db connection#
-sts9.config['MONGO_HOST'] = '10.0.0.36'
+sts9.config['MONGO_HOST'] = 'labops-bldr-lnx'
 sts9.config['MONGO_PORT'] = 27017
 sts9.config['MONGO_DBNAME'] = 'sts9_db'
 sts9_db = PyMongo(sts9, config_prefix='MONGO')
 
+#Global Variables
+now = datetime.datetime.now()
+
+
+
 #SETLISTS#
 @sts9.route('/setlists')
-def setlists():
-    setlists_data = sts9_db.db.setlists.find()
-    return render_template('setlists_nav.html', setlists_data=setlists_data)
+@sts9.route('/setlists/<year>')
+def setlists(year=None):
+    #finds all distinct years, for year navbar
+    years = sts9_db.db.setlists.distinct('year')
+    while True:
+        #set to most recent setlists as landing page
+        if year == None:
+            year = years[0]
+            break
+        #verify if it is a valid year url
+        elif year in years:
+            break
+        elif year not in years:
+            return render_template('404.html')
+    #render setlists template
+    setlists = sts9_db.db.setlists.find({'year': year})
+    return render_template('setlists_nav.html', years=years, setlists=setlists, year=year)
+
+
+    #object for current year shows
+
+
+
 
 #404 - Page Not Found Error Handler#
 @sts9.errorhandler(404)
